6b2f3cc5cccbe8750224d6a6f9c1b81ee420cf7c
Experconnect <nadjet.bouzid@experconnect.com>
2022-12-19 18:26:59 +0100

    affichage de l exemple dev hs

--- src/app/app.functions/assistant-card.js
+++ src/app/app.functions/assistant-card.js
@@ -3,6 +3,7 @@
 // Include HubSpot node API client
 const hubspot = require('@hubspot/api-client');
 
+/*
 exports.main = async (context = {}, sendResponse) => {
   // Store contact info, configured as propertiesToSend in crm-card.json
   const { hs_object_id, firstname, lastname, type_contact } = context.propertiesToSend;
@@ -15,3 +16,36 @@ exports.main = async (context = {}, sendResponse) => {
   };
   console.log(`${type_contact}`, `${type_contact}` === `Client`)
 };
+
+*/
+
+exports.main = async (context = {}, sendResponse) => {
+  const {propertiesToSend: { email, firstname, lifecyclestage }} = context;
+  try {
+   sendResponse({
+    title: "This card retrieves key contact details.",
+    sections: [
+     {
+      "type": "descriptionList",
+      "direction": "row",
+      "items": [
+       {
+        "label": "Email",
+        "value": email
+       },
+       {
+        "label": "First name",
+        "value": firstname
+       },
+       {
+        "label": "Lifecycle stage",
+        "value": lifecyclestage
+       }
+      ]
+     }
+    ]
+   });     
+  } catch (error) {
+     console.error(error);
+    }
+   };
