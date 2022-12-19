2f8882a48705ad8c4554de2501941e67b3e15ba1
Experconnect <nadjet.bouzid@experconnect.com>
2022-12-19 18:34:02 +0100

    sans crm card builder

--- src/app/app.functions/assistant-card.js
+++ src/app/app.functions/assistant-card.js
@@ -3,7 +3,22 @@
 // Include HubSpot node API client
 const hubspot = require('@hubspot/api-client');
 
+exports.main = async (context = {}, sendResponse) => {
+  // Store contact info, configured as propertiesToSend in crm-card.json
+  const { hs_object_id, firstname, lastname, type_contact } = context.propertiesToSend;
+  
+  if (`${type_contact}` === `Client`) {
+    if (`${nom_assistant_e}` !== ``) {
+      sendResponse({ sections: [`${nom_assistant_e}`], 
+    });
+    }
+  };
+  console.log(`${type_contact}`, `${type_contact}` === `Client`)
+};
 
+
+
+/*
 exports.main = async (context = {}, sendResponse) => {
   const {propertiesToSend: { email, firstname, lifecyclestage }} = context;
   try {
@@ -52,20 +67,6 @@ exports.main = async (context = {}, sendResponse) => {
      console.error(error);
     }
    };
-
-/*
-exports.main = async (context = {}, sendResponse) => {
-  // Store contact info, configured as propertiesToSend in crm-card.json
-  const { hs_object_id, firstname, lastname, type_contact } = context.propertiesToSend;
-  
-  if (`${type_contact}` === `Client`) {
-    if (`${nom_assistant_e}` !== ``) {
-      sendResponse({ sections: [`${nom_assistant_e}`], 
-    });
-    }
-  };
-  console.log(`${type_contact}`, `${type_contact}` === `Client`)
-};
-
 */
+
 
