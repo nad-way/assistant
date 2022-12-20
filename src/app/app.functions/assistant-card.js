b65be5794eb4ccf65f763b6c894c404a3f7323f4
Experconnect <nadjet.bouzid@experconnect.com>
2022-12-20 11:30:42 +0100

    key info sidebar card

--- src/app/app.functions/assistant-card.js
+++ src/app/app.functions/assistant-card.js
@@ -1,85 +1,38 @@
 // First des first 
 
+const hubspot = require('@hubspot/api-client');
 // For external API calls
-const axios = require("axios");
-
 exports.main = async (context = {}, sendResponse) => {
-  // Store contact firstname, configured as propertiesToSend in crm-card.json
-  const { firstname } = context.propertiesToSend;
-  
-  const introMessage = {
-    type: "alert",
-    title: "Your first UI extension is ready!",
-    variant: "success",
-    body: {
-      type: "text",
-      format: "markdown",
-      text: "Congratulations! You just deployed your first **HubSpot UI extension**. This example CRM card uses the [ZenQuotes public API](https://zenquotes.io/) to display a daily quote. It also includes custom button actions using serverless functions.",
-    },
-  };
-  
-  const nextSteps = [
-    {
-      type: "divider",
-      distance: "small",
-    },
-    {
-      type: "heading",
-      text: "Next Steps",
-    },
-    {
-      type: "text",
-      format: "markdown",
-      text: "You can explore more [code samples](https://github.com/HubSpot/ui-extensions-examples) or UI components in the [CRM card builder](https://app.hubspot.com/l/extensible-ui/), or try code samples. If you get stuck, take look at your [build or deploy logs](https://app.hubspot.com/l/developer-projects/) or [serverless functions logs](https://app.hubspot.com/l/private-apps/).",
-    },
-  ];
-  
+  const {propertiesToSend: { hs_object_id, nom_assistant_e, email, firstname }} = context; 
   try {
-    const { data } = await axios.get("https://zenquotes.io/api/random");
-
-    const quoteSections = [
-      {
-        type: "tile",
-        body: [
-          {
-            type: "text",
-            format: "markdown",
-            text: `**Hello ${firstname}, here's your quote for the day**!`,
-          },
-          {
-            type: "text",
-            format: "markdown",
-            text: `_${data[0].q}_`,
-          },
-          {
-            type: "text",
-            format: "markdown",
-            text: `_**Author**: ${data[0].a}_`,
-          },
-        ],
-      },
-      {
-        type: "button",
-        text: "Get new quote",
-        onClick: {
-          type: "SERVERLESS_ACTION_HOOK",
-          serverlessFunction: "crm-card",
+   sendResponse({
+    title: "This card retrieves key contact details.",
+    sections: [
+     {
+      "type": "descriptionList",
+      "direction": "row",
+      "items": [
+        {
+          "label": "ID",
+          "value": hs_object_id
         },
-      },
-    ];
-
-    sendResponse({
-      sections: [introMessage, ...quoteSections, ...nextSteps],
-    });
+        {
+          "label": "Nom Assistant",
+          "value": nom_assistant_e
+         }, 
+       {
+        "label": "Email",
+        "value": email
+       },
+       {
+        "label": "First name",
+        "value": firstname
+       }
+      ]
+     }
+    ]
+   });     
   } catch (error) {
-    // "message" will create an error feedback banner when it catches an error
-    sendResponse({
-      message: {
-        type: "ERROR",
-        body: `Error: ${error.message}`,
-      },
-      sections: [introMessage],
-    });
-  }
- 
-} 
+     console.error(error);
+    }
+   };
